b87045048db64b93d67f6edf941b0f8d821f5db2
BogdanGlushkov <91096839+BogdanGlushkov@users.noreply.github.com>
2021-10-29 14:09:16 +0500

    Add files via upload

---
+++ lab2.4.pas
@@ -0,0 +1,18 @@
+﻿var
+  number: string;
+
+var
+  i: integer;
+
+begin
+  writeln('Type a number consist of 0 and 1');
+  readln(number);
+  for i := 1 to length(number) do
+  begin
+    if(number[i] = '1') then
+      number[i] := '0'
+    else if(number[i] = '0') then
+      number[i] := '1';
+  end;
+  writeln(number);
+end.
