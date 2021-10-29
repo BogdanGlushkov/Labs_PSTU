0eec9941d6946dbe6105966b45765f86b7dbab1c
BogdanGlushkov <91096839+BogdanGlushkov@users.noreply.github.com>
2021-10-29 14:08:58 +0500

    Add files via upload

---
+++ lab2.1.py
@@ -0,0 +1,15 @@
+alph = ["a","b"]
+p = str(input())
+l = len(p)
+f = True
+
+for i in range(1,l):
+    if p[i] not in alph:
+        f = True
+        break
+    else:
+        f = False
+if f == True:
+    print("There is a symbol that is not in the alphabet")
+else:
+    print(p[1:])
