8222048b9c00cc069aba089fa33f3e32b9cb4dc0
BogdanGlushkov <91096839+BogdanGlushkov@users.noreply.github.com>
2021-12-13 19:29:52 +0500

    Add files via upload

---
+++ 2.18.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, newn = 0;
+    cout << "type random number - ";
+    cin >> n;
+    while (n != 0)
+    {
+        newn *= 10;
+        newn += n % 10;
+        n /= 10;
+    }
+    cout << "Result is: " << newn;
+}
---
+++ 2.20.cpp
@@ -0,0 +1,21 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, co = 0;
+    double a, n, z;
+    cout << "Type n, z: ";
+    cin >> n >> z;
+
+    for (i = 1; i <= n; i++)
+    {
+        a = 0;        
+        a = sin (n + n / i);
+        ++co;
+        a = round(a * 10) / 10;
+        if (z == a) { cout << "A = Z, if A = " << a << " and there is a " << co << "-d"<< endl; }
+        else { cout << "a" << co << " = " << a << endl; }
+    }
+}
