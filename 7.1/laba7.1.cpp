50b3e352c50bdbdbdfdbc3e477c8505bfc865624
BogdanGlushkov <91096839+BogdanGlushkov@users.noreply.github.com>
2022-02-26 22:45:01 +0500

    Add files via upload

---
+++ 7.1/laba7.1.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+using namespace std;
+
+float Add(float a1, float a2)
+{
+	return (a1 + a2);
+};
+
+float Add(float p1, float q1, float p2, float q2)
+//где p1, p2 - числители складываемых дробей, 
+//q- знаменатели складываемых дробей 
+{
+	return ((p1 / q1) + (p2 / q2));
+}
+
+int main()
+{
+	float a1, a2, res1, res2;
+	float p1, p2, q1, q2;
+	//
+	cout << "Type the tens numbers, that u would like to plus ^_^: ";
+	cin >> a1 >> a2;
+
+	cout << "Type the default slash numbers to sum it: ";
+	cin >> p1;
+	cout << "/";
+	cin >> q1;
+	cin >> p2;
+	cout << "/";
+	cin >> q2;
+
+	//res1 = Add(a1, a2);
+	//res2 = Add(p1, q1, p2, q2);
+
+	cout << Add(a1, a2) << " = " << a1 << " + " << a2 << endl;
+	cout << Add(p1, q1, p2, q2) << " = " << p1 << "/" << q1 << " + " << p2 << "/" << q2 << endl;
+}
